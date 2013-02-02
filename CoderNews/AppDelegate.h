d345e1d0280f9f28459a303045701d48d6285943
Matt Hodges <hodgesmr1@gmail.com>
2013-02-02 17:39:26 -0500

    Initial Commit

---
+++ CoderNews/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoderNews
+//
+//  Created by hodgesmr on 2/2/13.
+//  Copyright (c) 2013 Matt Hodges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoderNews/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  CoderNews
+//
+//  Created by hodgesmr on 2/2/13.
+//  Copyright (c) 2013 Matt Hodges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
