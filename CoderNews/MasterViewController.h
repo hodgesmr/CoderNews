166cac57ac2695fdd7acf699488c5f884577b366
Matt Hodges <hodgesmr1@gmail.com>
2013-02-02 18:07:13 -0500

    ripped out some auto-gened code

--- CoderNews/MasterViewController.h
+++ CoderNews/MasterViewController.h
@@ -8,11 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-#import <CoreData/CoreData.h>
+@interface MasterViewController : UITableViewController
 
-@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
-
-@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
-@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong) NSManagedObjectContext* managedObjectContext;
 
 @end
