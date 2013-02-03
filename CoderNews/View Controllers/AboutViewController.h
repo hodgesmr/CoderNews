467b4b143944d687b4d470de88fe0afb4ab8146c
Matt Hodges <hodgesmr1@gmail.com>
2013-02-03 11:32:55 -0500

    created about view

---
+++ CoderNews/View Controllers/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  CoderNews
+//
+//  Created by hodgesmr on 2/3/13.
+//  Copyright (c) 2013 Matt Hodges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *newsButton;
+
+@end
---
+++ CoderNews/View Controllers/AboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.m
+//  CoderNews
+//
+//  Created by hodgesmr on 2/3/13.
+//  Copyright (c) 2013 Matt Hodges. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
