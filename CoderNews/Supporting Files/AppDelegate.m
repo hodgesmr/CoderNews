3e1229e4df6187a822ab08f0ba5ecf1047a9e2a0
Matt Hodges <hodgesmr1@gmail.com>
2013-02-25 23:21:36 -0500

    I hated that dark nav bar

--- CoderNews/Supporting Files/AppDelegate.m
+++ CoderNews/Supporting Files/AppDelegate.m
@@ -15,15 +15,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    UIImage *navBackgroundImage = [UIImage imageNamed:@"navbar_bg"];
-    [[UINavigationBar appearance] setBackgroundImage:navBackgroundImage forBarMetrics:UIBarMetricsDefault];
     [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
                                                            [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], UITextAttributeTextColor,
                                                            [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8],UITextAttributeTextShadowColor,
                                                            [NSValue valueWithUIOffset:UIOffsetMake(0, 1)],
                                                            UITextAttributeTextShadowOffset,
                                                            [UIFont fontWithName:@"HelveticaNeue" size:21.0], UITextAttributeFont, nil]];
-    [[UIBarButtonItem appearance] setTintColor:[UIColor colorWithRed:32.0/255.0 green:32.0/255.0 blue:128.0/255 alpha:1.0]];
     return YES;
 }
 
