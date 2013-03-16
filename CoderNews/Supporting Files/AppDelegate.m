cbed5c6e874357b2fe196306fea75b289f858ef7
Matt Hodges <hodgesmr1@gmail.com>
2013-03-15 21:30:34 -0400

    custom toolbar background

--- CoderNews/Supporting Files/AppDelegate.m
+++ CoderNews/Supporting Files/AppDelegate.m
@@ -16,14 +16,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
-                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], UITextAttributeTextColor,
+                                                           [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0], UITextAttributeTextColor,
                                                            [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8],UITextAttributeTextShadowColor,
                                                            [NSValue valueWithUIOffset:UIOffsetMake(0, -1)],
                                                            UITextAttributeTextShadowOffset,
-                                                           [UIFont fontWithName:@"HelveticaNeue" size:21.0], UITextAttributeFont, nil]];
+                                                           [UIFont fontWithName:@"HelveticaNeue-Medium" size:21.0], UITextAttributeFont, nil]];
     [[UINavigationBar appearance] setTintColor:[UIColor darkGrayColor]];
     UIImage *navBackgroundImage = [UIImage imageNamed:@"navbar_bg"];
     [[UINavigationBar appearance] setBackgroundImage:navBackgroundImage forBarMetrics:UIBarMetricsDefault];
+    UIImage *toolbarBackgroundImage = [UIImage imageNamed:@"toolbar_bg"];
+    [[UIToolbar appearance] setBackgroundImage:toolbarBackgroundImage forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
     
     return YES;
 }
